3fb7a1bcfe36dc1ada63426f998baf3734048792
jl-do <doerzbacherjiaine@gmaill.com>
2023-06-22 18:03:58 -0300

    database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/resourses/CategoryResourse.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resourses/CategoryResourse.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class CategoryResourse {
     @Autowired
     private CategoryService service;
 
+    @Transactional(readOnly = true) // Para indicar que o método é somente leitura
     @GetMapping // Para indicar que o método responde a requisição do tipo GET do HTTP
     public ResponseEntity<List<Category>> findAll() {
         List<Category> list = service.findAll(); // Para acessar o serviço
