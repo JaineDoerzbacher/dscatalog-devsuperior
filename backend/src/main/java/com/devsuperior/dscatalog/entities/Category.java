377761d59541c4c1e9d011feb57e2c3568473c54
jl-do <doerzbacherjiaine@gmaill.com>
2023-06-22 19:40:09 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -12,6 +13,14 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Para indicar que o id é auto incrementado
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    // Para indicar que o tipo de dado é timestamp sem fuso horário
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    // Para indicar que o tipo de dado é timestamp sem fuso horário
+    private Instant updatedAt;
 
     public Category() {
     }
@@ -29,12 +38,30 @@ public class Category implements Serializable {
         return this.name;
     }
 
+    public Instant getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return this.updatedAt;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
