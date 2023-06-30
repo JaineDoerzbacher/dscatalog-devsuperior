f01246cdce28b1e6584f5ff318d1eb10afd7f641
jl-do <doerzbacherjiaine@gmaill.com>
2023-06-30 16:09:33 -0300

    Custom constraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -20,6 +20,7 @@ public class User implements Serializable {
     private String firstName;
     private String lastName;
 
+    @Column(unique = true) // Para garantir que o email será único
     private String email;
     private String password;
 
