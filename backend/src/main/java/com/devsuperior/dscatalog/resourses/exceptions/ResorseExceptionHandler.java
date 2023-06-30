38995c2ecbcd54f4198b76d9069008027db46fe3
jl-do <doerzbacherjiaine@gmaill.com>
2023-06-30 15:06:22 -0300

    Custom validation reponse

--- backend/src/main/java/com/devsuperior/dscatalog/resourses/exceptions/ResorseExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resourses/exceptions/ResorseExceptionHandler.java
@@ -4,6 +4,8 @@ import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourseNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -34,6 +36,23 @@ public class ResorseExceptionHandler {
         err.setError("Database exception"); // Para pegar o erro
         err.setMessage(e.getMessage()); // Para pegar a mensagem
         err.setPath(request.getRequestURI()); // Para pegar o caminho da requisição
+        return ResponseEntity.status(status).body(err);  // Para retornar a resposta com o status de erro do HTTP
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now()); // Para pegar o instante atual
+        err.setStatus(status.value()); // Para pegar o status
+        err.setError("Validation exception"); // Para pegar o erro
+        err.setMessage(e.getMessage()); // Para pegar a mensagem
+        err.setPath(request.getRequestURI()); // Para pegar o caminho da requisição
+
+        for (FieldError f :e.getBindingResult().getFieldErrors()){ // Para percorrer a lista de erros
+           err.addError(f.getField(), f.getDefaultMessage()); // Para adicionar o erro na lista de erros
+
+        }
         return ResponseEntity.status(status).body(err);  // Para retornar a resposta com o status de erro do HTTP
     }
 
